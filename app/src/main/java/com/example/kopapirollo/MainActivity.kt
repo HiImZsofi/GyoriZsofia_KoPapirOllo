e4c452fd3a73f0d94a5be52251136e0df59cd59a
HiImZsofi <zsofia.gyori00@gmail.com>
2022-10-15 12:09:55 +0200

    fix bug where wins and losses are wrongly counted

--- app/src/main/java/com/example/kopapirollo/MainActivity.kt
+++ app/src/main/java/com/example/kopapirollo/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.kopapirollo
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
@@ -42,11 +44,13 @@ class MainActivity : AppCompatActivity() {
             rnd = (1..3).random()
             if(rnd == 1){ //player rock comp rock
                 draws++
+                println("Kő draw" + draws)
                 compPick.setImageResource(R.drawable.rock)
                 draw.setText("Döntetlenek száma: $draws")
             }
             else if(rnd == 3){ //player rock comp scissors
                 win++
+                println("Kő win" + win)
                 compPick.setImageResource(R.drawable.scissors)
                 Toast.makeText(this@MainActivity, "Nyertél!", Toast.LENGTH_SHORT).show()
                 if(win == 1){
@@ -62,6 +66,7 @@ class MainActivity : AppCompatActivity() {
             }
             else if(rnd == 2){ //player rock comp paper
                 loss++
+                println("Kő lostt " + loss)
                 compPick.setImageResource(R.drawable.paper)
                 Toast.makeText(this@MainActivity, "Veszítettél!", Toast.LENGTH_SHORT).show()
                 if(loss == 1){
@@ -82,35 +87,38 @@ class MainActivity : AppCompatActivity() {
             rnd = (1..3).random()
             if(rnd == 2){ //player paper comp paper
                 draws++
+                println("Papir draw" + draw)
                 compPick.setImageResource(R.drawable.paper)
                 draw.setText("Döntetlenek száma: $draws")
             }
             else if(rnd == 3){ //player paper comp scissors
                 loss++
+                println("Papir loss" + loss)
                 compPick.setImageResource(R.drawable.scissors)
                 Toast.makeText(this@MainActivity, "Veszítettél!", Toast.LENGTH_SHORT).show()
-                if(win == 1){
+                if(loss == 1){
                     pheart1.setImageResource(R.drawable.heart1)
                 }
-                else if(win == 2){
+                else if(loss == 2){
                     pheart2.setImageResource(R.drawable.heart1)
                 }
-                else if(win == 3){
+                else if(loss == 3){
                     pheart3.setImageResource(R.drawable.heart1)
                     gameOver()
                 }
             }
             else if(rnd == 1){ //player paper comp rock
                 win++
+                println("Papir win" + win)
                 compPick.setImageResource(R.drawable.rock)
                 Toast.makeText(this@MainActivity, "Nyertél!", Toast.LENGTH_SHORT).show()
-                if(loss == 1){
+                if(win == 1){
                     cheart3.setImageResource(R.drawable.heart1)
                 }
-                else if(loss == 2){
+                else if(win == 2){
                     cheart2.setImageResource(R.drawable.heart1)
                 }
-                else if(loss == 3){
+                else if(win == 3){
                     cheart1.setImageResource(R.drawable.heart1)
                     gameOver()
                 }
@@ -122,35 +130,38 @@ class MainActivity : AppCompatActivity() {
             rnd = (1..3).random()
             if(rnd == 3){ //player scissors comp scissors
                 draws++
+                println("Ollo draw" + draw)
                 compPick.setImageResource(R.drawable.scissors)
                 draw.setText("Döntetlenek száma: $draws")
             }
             else if(rnd == 1){ //player scissors comp rock
                 loss++
+                println("Ollo loss" + loss)
                 compPick.setImageResource(R.drawable.rock)
                 Toast.makeText(this@MainActivity, "Veszítettél!", Toast.LENGTH_SHORT).show()
-                if(win == 1){
+                if(loss == 1){
                     pheart1.setImageResource(R.drawable.heart1)
                 }
-                else if(win == 2){
+                else if(loss == 2){
                     pheart2.setImageResource(R.drawable.heart1)
                 }
-                else if(win == 3){
+                else if(loss == 3){
                     pheart3.setImageResource(R.drawable.heart1)
                     gameOver()
                 }
             }
             else if(rnd == 1){ //player scissors comp paper
                 win++
+                println("Ollo win " + win)
                 compPick.setImageResource(R.drawable.paper)
                 Toast.makeText(this@MainActivity, "Nyertél!", Toast.LENGTH_SHORT).show()
-                if(loss == 1){
+                if(win == 1){
                     cheart3.setImageResource(R.drawable.heart1)
                 }
-                else if(loss == 2){
+                else if(win == 2){
                     cheart2.setImageResource(R.drawable.heart1)
                 }
-                else if(loss == 3){
+                else if(win == 3){
                     cheart1.setImageResource(R.drawable.heart1)
                     gameOver()
                 }
@@ -160,7 +171,47 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun gameOver(){
-
+        var gameOverDialogBuilder = AlertDialog.Builder(this@MainActivity)
+        if(win > loss){
+            gameOverDialogBuilder.setTitle("Győzelem")
+            gameOverDialogBuilder.setMessage("Szeretne új játékot játszani?")
+            gameOverDialogBuilder.setCancelable(false)
+            gameOverDialogBuilder.setNegativeButton("Nem", DialogInterface.OnClickListener {
+                    dialog, id -> finish()
+            })
+            gameOverDialogBuilder.setPositiveButton("Igen", DialogInterface.OnClickListener {
+                    dialog, id -> draws = 0
+                win = 0
+                loss = 0
+                cheart1.setImageResource(R.drawable.heart2)
+                cheart2.setImageResource(R.drawable.heart2)
+                cheart3.setImageResource(R.drawable.heart2)
+                pheart1.setImageResource(R.drawable.heart2)
+                pheart2.setImageResource(R.drawable.heart2)
+                pheart3.setImageResource(R.drawable.heart2)
+            })
+            gameOverDialogBuilder.show()
+        }
+        else{
+            gameOverDialogBuilder.setTitle("Vereség")
+            gameOverDialogBuilder.setMessage("Szeretne új játékot játszani?")
+            gameOverDialogBuilder.setCancelable(false)
+            gameOverDialogBuilder.setNegativeButton("Nem", DialogInterface.OnClickListener {
+                    dialog, id -> finish()
+            })
+            gameOverDialogBuilder.setPositiveButton("Igen", DialogInterface.OnClickListener {
+                    dialog, id -> draws = 0
+                win = 0
+                loss = 0
+                cheart1.setImageResource(R.drawable.heart2)
+                cheart2.setImageResource(R.drawable.heart2)
+                cheart3.setImageResource(R.drawable.heart2)
+                pheart1.setImageResource(R.drawable.heart2)
+                pheart2.setImageResource(R.drawable.heart2)
+                pheart3.setImageResource(R.drawable.heart2)
+            })
+            gameOverDialogBuilder.show()
+        }
     }
 
     fun init(){
